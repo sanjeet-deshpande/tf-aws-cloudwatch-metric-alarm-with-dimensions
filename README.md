a6a91d9cf906c75ee38b840fdc3e4078637baa31
deshpas <sanjeet.deshpande@autodesk.com>
2018-05-28 00:26:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## AWS CLOUDWATCH METRIC ALARM
+===========
+
+### tf_aws_cloudwatch_metric_alarm_with_dimensions 
+ Terraform module for creating AWS CLOUDWATCH METRIC ALARM for individual AWS resources like InstanceId,InstanceType,ImageId, DBInstanceIdentifier, DBClusterIdentifier etc..
+
+### Pre-Requisite:
+* Please create a SNS topic as Email before start using this.
+* We are going to use SNS topic ARN for notification while creating Cloudwatch Metric Alarm.
+
+### How do we use it ?
+
+#### Pre-Requisite:
+ - Please create a SNS topic as Email before start using this.
+ - We are going to use SNS topic ARN for notification while creating Cloudwatch Metric Alarm.
+
+The input variables and output parameters are documented in
+ - [tf_aws_cloudwatch_metric_alarm_with_dimensions/TERRAFORM.md](https://github.com/sanjeet-deshpande/tf-aws-cloudwatch-metric-alarm-with-dimensions/blob/master/TERRAFORM.md)
+
---
+++ main.tf
@@ -0,0 +1,24 @@
+data "template_file" "cloudwatch_metric_alarm_prefix_tpl" {
+  template = "$${product}_$${environment}"
+
+  vars {
+    product         = "${var.comp_vars["product"]}"
+    environment          = "${var.environment}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "main" {
+  alarm_name        = "${data.template_file.cloudwatch_metric_alarm_prefix_tpl.rendered}_${var.alarm_name}"
+  alarm_description = "Managed By Terraform - ${var.comp_vars["product"]} ${var.environment} ${var.alarm_name}"
+  metric_name       = "${var.metric_name}"
+  comparison_operator = "${var.comparison_operator}"
+  evaluation_periods  = "${var.evaluation_periods}"
+  namespace           = "${var.namespace}"
+  period 	      = "${var.statistics_period}"
+  statistic           = "${var.statistic}"
+  threshold           = "${var.threshold}"
+  actions_enabled     = "${var.actions_enabled}"
+  alarm_actions       = ["${var.alarm_action_arn}"]
+  ok_actions          = ["${var.ok_action_arn}"]
+  dimensions          = "${var.metric_dimensions}"
+}
